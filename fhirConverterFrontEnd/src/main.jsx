2a221e114510cf1ceed6915342be67bd7119c5cb
jushen dhillon <jushendhillon@London-Macbook.local>
2024-08-08 17:38:07 -0400

    downgraded version of react to be compatible with oauth login

--- fhirConverterFrontEnd/src/main.jsx
+++ fhirConverterFrontEnd/src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.jsx'
-import Login from "./Login.jsx"
-import PatientPage from "./PatientPage.jsx"
+import App from './App.jsx';
+import Login from './Login.jsx';
+import PatientPage from './PatientPage.jsx';
 
 const router = createBrowserRouter([
   {
@@ -18,15 +18,16 @@ const router = createBrowserRouter([
   {
     path: '/convertPage',
     element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>
+    errorElement: <h1 className='display-2'>Wrong page!</h1>,
   },
   {
-    path: "/patientPage",
+    path: '/patientPage',
     element: <PatientPage />,
     errorElement: <h1 className='display-2'>Wrong page!</h1>,
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
-)
+ReactDOM.render(
+  <RouterProvider router={router} />,
+  document.getElementById('root')
+);
